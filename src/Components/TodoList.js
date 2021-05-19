7d6954a9254679c9dd7e754f634542f3cd7a1f14
mrkau99 <mrkaumohanty@gmail.com>
2021-05-19 19:09:14 +0530

    Todo

---
+++ src/Components/TodoList.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Todo from "./Todo";
+import "../App.css";
+import TodoForm from "./TodoForm";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+    console.log(todos, ...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item)),
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = Todo.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+  let today = new Date();
+  let date =
+    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+  return (
+    <>
+      <h1>Create Todos for {date}</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+};
+
+export default TodoList;
